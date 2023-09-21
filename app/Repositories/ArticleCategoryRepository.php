397edad4a0af92479eca6bd2f6215dd43196cdf2
Ahmadreza <ahmadrezabashari@gmail.com>
2023-09-21 14:40:54 +0330

    Optimized where method in ArticleCategoryRepository and added delete method

--- app/Repositories/ArticleCategoryRepository.php
+++ app/Repositories/ArticleCategoryRepository.php
@@ -16,9 +16,9 @@ class ArticleCategoryRepository
         return ArticleCategory::create($data);
     }
 
-    public static function where($id)
+    public static function where(array $conditions)
     {
-        return ArticleCategory::where('id', '=' ,$id)->first();
+        return ArticleCategory::where($conditions)->first();
     }
 
     public static function exists($id)
@@ -29,6 +29,11 @@ class ArticleCategoryRepository
     public static function update($id, $data)
     {
         return ArticleCategory::where('id', '=' ,$id)->update($data);
+    }
+
+    public static function delete($code)
+    {
+        return ArticleCategory::where('category_code', '=' ,$code)->delete();
     }
 }
 ?>
